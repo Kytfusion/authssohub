4181371aa8069d49c0b67de014740a012e125cb4
Nichita <bespalco.nn@gmail.com>
2025-07-12 19:27:12 +0300

    Refactored user registration process by removing CreateProfileController and UserProfileController. Updated RegisterController to handle user registration, validation, and token generation, enhancing overall functionality and streamlining the codebase.

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,14 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Core\ProfileCore;
+use App\Models\ProfileModel;
+use App\Services\ProfileService;
+use Illuminate\Support\Facades\Hash;
+use Exception;
 
 class RegisterController extends Controller
 {
     public function register(
         ProfileCore $profileCore,
+        ProfileService $profileService,
+        ProfileModel $profileModel
     ) {
         try {
-            $profileCore->register_user();
+            $credentials = request()->validate($profileService->validate);
+
+            $user = ProfileModel::where(self::FIELD26, $credentials[self::FIELD26])->first();
+
+            if ($user) {
+                $profileCore->user = null;
+                throw new Exception('Email exists');
+            }
+
+            $profileModel->{self::FIELD26} = $credentials[self::FIELD26];
+            $profileModel->{self::FIELD27} = Hash::make($credentials[self::FIELD27]);
+            $profileModel->save();
+
+            $profileCore->user = $profileModel;
+
+            $profileCore->new_refreshToken();
+            $profileCore->new_accessToken();
+
+            $profileModel->{self::FIELD41} = $profileCore->refreshToken;
+            $profileModel->save();
 
             return response()->json([
                 'access_token' => $profileCore->accessToken,
